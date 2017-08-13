ba7d3fbca59bb722c708d90d2db738aae65ff168
liubin192837 <2928981043@qq.com>
2017-08-13 21:51:20 +0800

    Test

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+ /*   BlankFragment  Fragment1 =null;
+    BlankFragment  Fragment2 =null;*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+/*        Fragment1 = new BlankFragment();
+        Fragment2 = new BlankFragment();*/
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
